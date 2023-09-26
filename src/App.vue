5482cbd13f6b541332cc3fd37211f419778fdb90
Simone <sdiricco.91@gmail.com>
2023-09-26 18:33:50 +0200

    try keyboard

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,38 @@
 <template>
-  <ion-app>
-    <ion-router-outlet />
+  <ion-app :class="{ 'keyboard-open': isKeyboardOpen }">
+    <ion-router-outlet/>
   </ion-app>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import { ref, onMounted, onUnmounted } from 'vue';
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    IonApp,
-    IonRouterOutlet
-  }
+const isKeyboardOpen = ref(false);
+let windowHeight = window.innerHeight;
+
+const calculateKeyboardHeight = () => {
+  const newWindowHeight = window.innerHeight;
+  const keyboardHeight = windowHeight - newWindowHeight;
+  document.documentElement.style.setProperty('--keyboard-height', `${keyboardHeight}px`);
+  isKeyboardOpen.value = keyboardHeight > 0;
+};
+
+onMounted(() => {
+  window.addEventListener('resize', calculateKeyboardHeight);
+  calculateKeyboardHeight();
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', calculateKeyboardHeight);
 });
 </script>
+
+<style scoped>
+
+/* Stile per il corpo quando la tastiera è attiva */
+.keyboard-open {
+  height: calc(100% - var(--keyboard-height));
+  overflow: hidden;
+}
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,7 +2,7 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Doctor Ai</ion-title>
+        <ion-title>Doctor Ai v0.0.1</ion-title>
       </ion-toolbar>
     </ion-header>
 
